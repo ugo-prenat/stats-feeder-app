c4903e99c7cedc1359fc3146313b055f7414643b
Ugo <ugo.prenat@gmail.com>
2022-11-05 22:06:59 +0100

    changing

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,13 +5,13 @@ const DB_USERNAME = process.env.DB_USERNAME || '';
 const DB_PASSWORD = process.env.DB_PASSWORD || '';
 const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.9efmlja.mongodb.net/stats-feeder`;
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 4000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 
 export const config = {
   mongo: {
     url: DB_URL
   },
   server: {
-    port: SERVER_PORT
+    port: PORT
   }
 }
