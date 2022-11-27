1761c1a994bc4f796ab71076930b6c56f9738637
Ugo <ugo.prenat@gmail.com>
2022-11-27 20:31:56 +0100

    remove server logs on prod environment

--- server/src/utils/Logging.ts
+++ server/src/utils/Logging.ts
@@ -4,17 +4,21 @@ export default class Logging {
   public static log = (args: any) => this.info(args)
   public static info = (args: any) =>
     console.log(
-      chalk.blue(`[${new Date().toLocaleString()}] [INFO]`),
+      chalk.blue(`${getDate()}[INFO]`),
       typeof args === 'string' ? chalk.blueBright(args) : args
     )
   public static warn = (args: any) =>
     console.log(
-      chalk.yellow(`[${new Date().toLocaleString()}] [WARN]`),
+      chalk.yellow(`${getDate()}[WARN]`),
       typeof args === 'string' ? chalk.yellowBright(args) : args
     )
   public static error = (args: any) =>
     console.log(
-      chalk.red(`[${new Date().toLocaleString()}] [ERROR]`),
+      chalk.red(`${getDate()}[ERROR]`),
       typeof args === 'string' ? chalk.redBright(args) : args
     )
+}
+const getDate = () => {
+  if (process.env.ENV === 'DEV') return `[${new Date().toLocaleString()}] `
+  return ''
 }
