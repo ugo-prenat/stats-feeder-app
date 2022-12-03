8154d0135fade43e4d75295d836c87be5aaff77c
Ugo <ugo.prenat@gmail.com>
2022-12-03 18:59:39 +0100

    notif system

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
 import './styles/export.scss'
+import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- client/src/pages/onboarding/BotDataForm.tsx
+++ client/src/pages/onboarding/BotDataForm.tsx
@@ -4,12 +4,14 @@ import PrimaryBtn from '../../components/buttons/PrimaryBtn'
 import { FiArrowRight as RightArrowIcon } from 'react-icons/fi'
 import TwitterUsernameInput from '../../components/forms/TwitterUsernameInput'
 import { useForm, Controller } from 'react-hook-form'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 import { IProfileImg } from './OnboardingStage0'
-import { request } from '../../utils/request'
-import { IAPIBot } from '../../models/bot.model'
+import { req } from '../../utils/request'
+import { IBotResponse } from '../../models/bot.model'
+import { LangContext } from '../../components/providers/LangContextProvider'
+import { ToastContainer, toast } from 'react-toastify'
 
 type BotDataFormProps = {
   setName: (name: string) => void
@@ -47,80 +49,103 @@ const BotDataForm: React.FC<BotDataFormProps> = ({
     setError,
     formState: { errors }
   } = useForm<BotDataFormValues>({ resolver: yupResolver(schema) })
+  const { getText } = useContext(LangContext)
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const onSubmit = handleSubmit((data) => {
+  const onSubmit = handleSubmit(data => {
     setIsSubmitting(true)
     const itContainsImg = data.profileImg !== undefined
 
     const formData = new FormData()
     if (itContainsImg) formData.append('files', data.profileImg.data)
 
-    request('POST', `/bots?name=${data.name}&username=${data.username}`, formData, itContainsImg)
-      .then((res) => {
-        const bot: IAPIBot = res.bot
-        localStorage.setItem('botId', bot._id)
+    req<IBotResponse>(
+      'POST',
+      `/bots?name=${data.name}&username=${data.username}`,
+      itContainsImg ? formData : undefined
+    )
+      .then(res => {
+        if (!res.bot) return toast.error(getText('request.error'))
+        localStorage.setItem('botId', res.bot._id)
         nextStage()
       })
-      .catch((err) => console.log(err))
+      .catch(err => {
+        toast.error(getText('request.error'))
+        console.log(err)
+      })
       .finally(() => setIsSubmitting(false))
   })
 
   return (
-    <form onSubmit={onSubmit}>
-      <Controller
-        control={control}
-        name="name"
-        render={({ field: { onChange, value } }) => (
-          <Input
-            label={'label.name'}
-            value={value}
-            onChange={(e) => {
-              setName(e.currentTarget.value)
-              onChange(e.currentTarget.value)
-            }}
-            error={errors.name?.message}
-          />
-        )}
+    <>
+      <form onSubmit={onSubmit}>
+        <Controller
+          control={control}
+          name="name"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              label={'label.name'}
+              value={value}
+              onChange={e => {
+                setName(e.currentTarget.value)
+                onChange(e.currentTarget.value)
+              }}
+              error={errors.name?.message}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="username"
+          render={({ field: { onChange, value } }) => (
+            <TwitterUsernameInput
+              username={value}
+              setUsername={e => {
+                setUsername(e.currentTarget.value)
+                onChange(e.currentTarget.value)
+              }}
+              error={errors.username?.message}
+              setError={setError}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="profileImg"
+          render={({ field: { onChange } }) => (
+            <ImgInput
+              value={'label.profilePicture'}
+              preview={previewImg}
+              setProfileImg={({ data, preview }: IProfileImg) => {
+                setProfileImg({ data, preview })
+                onChange({ data, preview })
+              }}
+              error={errors.profileImg?.message}
+              setError={setError}
+            />
+          )}
+        />
+        <PrimaryBtn
+          text="btn.nextStep"
+          icon={<RightArrowIcon />}
+          iconPosition="right"
+          disabled={isSubmitting}
+        />
+      </form>
+
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
       />
-      <Controller
-        control={control}
-        name="username"
-        render={({ field: { onChange, value } }) => (
-          <TwitterUsernameInput
-            username={value}
-            setUsername={(e) => {
-              setUsername(e.currentTarget.value)
-              onChange(e.currentTarget.value)
-            }}
-            error={errors.username?.message}
-            setError={setError}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="profileImg"
-        render={({ field: { onChange } }) => (
-          <ImgInput
-            value={'label.profilePicture'}
-            preview={previewImg}
-            setProfileImg={({ data, preview }: IProfileImg) => {
-              setProfileImg({ data, preview })
-              onChange({ data, preview })
-            }}
-            error={errors.profileImg?.message}
-            setError={setError}
-          />
-        )}
-      />
-      <PrimaryBtn
-        text="btn.nextStep"
-        icon={<RightArrowIcon />}
-        iconPosition="right"
-        disabled={isSubmitting}
-      />
-    </form>
+    </>
   )
 }
 
